0737ae7c086cc716a5addf40e52820bc09f4c8e4
Michele Otta <micheleotta@gmail.com>
2025-06-09 00:27:43 -0300

    create graphs

---
+++ main.py
@@ -0,0 +1,135 @@
+from collections import defaultdict
+import pandas as pd
+
+# Grafo ponderado -> lista de adjacências
+class Grafo:
+
+    # construtor
+    def __init__(self, tipo):
+        self.ordem = 0
+        self.tamanho = 0
+        self.adj_list = defaultdict(list)
+        self.direcionado = tipo
+
+    def __str__(self):
+        return self.imprime_lista_adjacencias()
+
+    def adiciona_vertice(self, u): # adiciona um vértice u (rotulado) ao grafo G.
+        # não permite a inclusão de vértices repetidos.
+        if self.tem_vertice(u):
+            print(f"\nVértice {u} já foi adicionado.\n")
+            return
+        else:
+            self.adj_list[u] = []
+            self.ordem += 1
+
+    def adiciona_aresta(self, u, v): # cria uma aresta com peso positivo entre os vértices u e v do grafo G.
+        # caso algum dos vértices não exista durante a criação da aresta, ele deve ser criado no grafo.
+        if not self.tem_vertice(u):
+            self.adiciona_vertice(u)
+        if not self.tem_vertice(v):
+            self.adiciona_vertice(v)
+
+        # caso a aresta já exista, ela deve ser atualizada com o novo peso.
+        for i, (id, peso_atual) in enumerate(self.adj_list[u]):
+            if id == v:
+                novo_peso = peso_atual + 1
+                self.adj_list[u][i] = (v, novo_peso)
+                if not self.direcionado: # se não direcionado criar para o outro sentido também v -> u
+                    for j, (id2, _) in enumerate(self.adj_list[v]):
+                        if id2 == u:
+                            self.adj_list[v][j] = (u, novo_peso)
+                            return
+                return
+
+        # se não existir, cria
+        self.adj_list[u].append((v, 1))
+        if not self.direcionado:
+            self.adj_list[v].append((u, 1))
+        self.tamanho += 1
+
+    def tem_vertice(self, u): # verifica se o vértice u existe no grafo G e retorna True ou False.
+        if u in self.adj_list:
+            return True
+        return False
+
+    def grau_entrada(self, u): # retorna a quantidade total de arestas que chegam até o vértice u do grafo G.
+        if not self.tem_vertice(u):
+            print(f"\nVértice {u} não existe. Não foi possível calcular o grau de entrada.\n")
+            return None
+        grau_entrada = 0
+        for vertice in self.adj_list:
+            for item in self.adj_list[vertice]:
+                if item[0] == u:
+                    grau_entrada += 1
+        return grau_entrada
+
+    def grau_saida(self, u): # retorna a quantidade total de arestas que saem do vértice u do grafo G
+        if not self.tem_vertice(u):
+            print(f"\nVértice {u} não existe. Não foi possível calcular o grau de saida.\n")
+            return None
+        grau_saida = len(self.adj_list[u])
+        return grau_saida
+
+    def grau(self, u): # retorna a quantidade total de arestas conectadas (indegree + outdegree) ao vértice u do grafo G
+        if not self.tem_vertice(u):
+            print(f"\nVértice {u} não existe. Não foi possível calcular seu grau.\n")
+            return None
+    
+        if self.direcionado:
+            return self.grau_entrada(u) + self.grau_saida(u)
+        else: # se não direcionado
+            return len(self.adj_list[u])
+
+    def get_peso(self, u, v): # retorna qual é o peso da aresta entre os vértices u e v do grafo G, caso exista uma aresta entre eles
+        for vertice in self.adj_list[u]:
+            if vertice[0] == v:
+                return vertice[1]
+        return None
+
+    def imprime_lista_adjacencias(self):
+        print("\n⋅˚₊‧ Lista de adjacências ‧₊˚ ⋅")
+        for vertice in self.adj_list:
+            print(f"{vertice}: ", end="")
+            for adjacente in self.adj_list[vertice]:
+                print(f"{adjacente}", end=" -> ")
+            print()
+        print("\n")
+        return ""
+
+
+# ler o dataset
+df = pd.read_csv('netflix_amazon_disney_titles.csv')
+df = df[['director', 'cast']]
+df.dropna(subset=['director', 'cast'], inplace=True) # ignorar entradas vazias
+df.head()
+
+# formato padronizado dos nomes
+def padronizar_string(string):
+    string = string.upper().strip()
+    return string
+
+# construir os grafos
+G1 = Grafo(True)  # grafo direcionado
+G2 = Grafo(False) # grafo não direcionado
+for id, cast in enumerate(df['cast']):
+    actors_list = cast.split(', ')
+    actors = [padronizar_string(actor) for actor in actors_list]
+    for id_a, actor in enumerate(actors):
+        # adicionar no grafo direcionado(actor, director)
+        # actor -> director
+        director_list = (df['director'].iloc[id]).split(', ')
+        directors = [padronizar_string(director) for director in director_list]
+        for director in (directors):
+            G1.adiciona_aresta(actor, director)
+        # adicionar grafo não direcionado relação entre atores
+        for i in range(id_a + 1, len(actors)):
+            G2.adiciona_aresta(actor, actors[i])
+
+print('Grafo 1 (direcionado, ator -> diretor)')
+print('Ordem =', G1.ordem)
+print('Tamanho =', G1.tamanho)
+
+print('\nGrafo 2 (não direcionado, ator -> ator)')
+print('Ordem = =', G2.ordem)
+print('Tamanho =', G2.tamanho)
